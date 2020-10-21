e62af12d2d9868787fc256e315b3a48ea78e4c91
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-21 10:18:34 +0530

    added: Created InvoiceGenerator to calculate Fare

--- main/java/com/capgemini/CabInvoiceGenerator/InvoiceGenerator.java
+++ main/java/com/capgemini/CabInvoiceGenerator/InvoiceGenerator.java
@@ -1,7 +1,14 @@
 package com.capgemini.CabInvoiceGenerator;
 
 public class InvoiceGenerator {
-public static void main(String[] args) {
-	System.out.println("Welcome to the Cab Invoice Generator");
-}
+	private static final double MINIMUM_COST_PER_KM = 10.0;
+	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5.0;
+
+	public static double calculateFare(double distance, int time) {
+		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+		if(totalFare<MINIMUM_FARE)
+			return MINIMUM_FARE;
+		return totalFare;
+	}
 }

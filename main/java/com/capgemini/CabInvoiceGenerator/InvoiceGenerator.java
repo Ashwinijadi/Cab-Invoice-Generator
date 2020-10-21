0f81abc34e002deb32a7533a15239c63b05ad279
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-21 10:00:04 +0530

    added: Welcome to the Cab Invoice Generator Programming

---
+++ main/java/com/capgemini/CabInvoiceGenerator/InvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.capgemini.CabInvoiceGenerator;
+
+public class InvoiceGenerator {
+public static void main(String[] args) {
+	System.out.println("Welcome to the Cab Invoice Generator");
+}
+}

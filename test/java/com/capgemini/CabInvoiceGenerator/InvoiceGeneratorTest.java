89c969121c3d46cf3371bc9d9acd9dfc3e41ae6e
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-21 10:19:40 +0530

    added: functionality of testing fare is added

---
+++ test/java/com/capgemini/CabInvoiceGenerator/InvoiceGeneratorTest.java
@@ -0,0 +1,21 @@
+package com.capgemini.CabInvoiceGenerator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvoiceGeneratorTest {
+	@Test
+	public void givenLessDistanceAndTimeShouldReturnMinimumFare() {
+		double distance = 0.1;
+		int time = 1;
+		double fare = InvoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+	}
+	@Test
+	public void givenDistanceAndTimeShouldReturnTotalFare() {
+		double distance = 2;
+		int time = 5;
+		double fare = InvoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(25, fare, 0.0);
+	}
+}
